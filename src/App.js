6208187a4c8d4f552094cded74fb1184f1b52a63
nik-mcknight <nik.mcknight735@gmail.com>
2023-07-15 14:55:43 -0600

    Initial commit. Mostly complete.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Seal from "./Seal";
+import State from "./State";
+import City from "./City";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Seal />
+        <h1>Iowa</h1>
       </header>
+      <nav>
+        <Link className="navLink" to="/">
+          Home
+        </Link>
+        <Link className="navLink" to="/DesMoines">
+          Des Moines
+        </Link>
+        <Link className="navLink" to="/IowaCity">
+          Iowa City
+        </Link>
+        <Link className="navLink" to="/Ames">
+          Ames
+        </Link>
+        <Link className="navLink" to="/CedarFalls">
+          Cedar Falls
+        </Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<State />}></Route>
+        <Route path="/DesMoines" element={<City name="Des Moines" />}></Route>
+        <Route path="/IowaCity" element={<City name="Iowa City" />}></Route>
+        <Route path="/Ames" element={<City name="Ames" />}></Route>
+        <Route path="/CedarFalls" element={<City name="Cedar Falls" />}></Route>
+      </Routes>
+      <footer></footer>
+      {/* <Header />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/account" element={<Account />}></Route>
+        <Route path="/admin" element={<Admin />}></Route>
+        <Route path="/update" element={<Update />}></Route>
+        <Route path="/reboot" element={<Reboot />}></Route>
+        <Route path="/help" element={<Help />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/termsOfService" element={<ToS />}></Route>
+      </Routes>
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/City.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function City({ name }) {
+  return (
+    <div className="Page">
+      <h2>{name}</h2>
+      <div className="Content">hello</div>
+    </div>
+  );
+}
