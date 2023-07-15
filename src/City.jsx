56e75028a8ed45d82ec9ade4f2ed9563e9e9f7c6
nik-mcknight <nik.mcknight735@gmail.com>
2023-07-15 15:49:16 -0600

    Added all city data

--- src/City.jsx
+++ src/City.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 
-export default function City({ name }) {
+export default function City({
+  name,
+  image,
+  population,
+  yearInc,
+  region,
+  classif,
+  income,
+  blurb,
+}) {
   return (
     <div className="Page">
       <h2>{name}</h2>
-      <div className="Content">hello</div>
+      <div className="Content">
+        <section className="cityContent">{blurb}</section>
+        {image}
+        <aside className="cityAside">
+          <p>Population: {population}</p>
+          <p>Year Incorporated: {yearInc}</p>
+          <p>Region: {region}</p>
+          <p>Classification: {classif}</p>
+          <p>Per capita income (county): {income} / 99</p>
+        </aside>
+      </div>
     </div>
   );
 }
